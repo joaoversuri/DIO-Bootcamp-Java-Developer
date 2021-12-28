fe9534f6a1f14ceeeb4b5a38eb8aed4ebf2c2bed
joaoversuri <joaopaulo.db.versuri@gmail.com>
2021-12-27 22:41:56 -0300

    Retorno no metodo quadrilatero e criação de menu de seleção de operação  para o metodo calculadora.

--- metodos/src/com/metodos/Main.java
+++ metodos/src/com/metodos/Main.java
@@ -11,10 +11,8 @@ public class Main {
 
     public static void main(String[] args) {
         int opcao;
-        boolean shouldBreak = false;
         Scanner scan = new Scanner(System.in);
-        inicio:
-        while (!shouldBreak) {
+        inicio:while (true) {
             System.out.println("Escolha uma opção:");
             System.out.println("1 - Calculadora");
             System.out.println("2 - Horário");
@@ -26,15 +24,50 @@ public class Main {
                 case 1 -> {
                     // Calculadora
                     System.out.println("Exercício calculadora");
-                    Calculadora.soma(3, 6);
-                    Calculadora.subtracao(9, 1.8);
-                    Calculadora.multiplicacao(7, 8);
-                    Calculadora.divisao(5, 2.5);
+                    calculadora:while(true) {
+                        System.out.println("Selecione a operação:");
+                        System.out.println("1 - Adição");
+                        System.out.println("2 - Subtração");
+                        System.out.println("3 - Multiplicação");
+                        System.out.println("4 - Divisão");
+                        System.out.println("5 - Retornar ao menu anterior");
+                        int operacao = scan.nextInt();
+                        System.out.println("Digite o primeiro valor:");
+                        double numero1= scan.nextDouble();
+                        System.out.println("Digite o segundo valor:");
+                        double numero2= scan.nextDouble();
+                        System.out.println();
+                        switch (operacao) {
+                            case 1 -> {
+                                Calculadora.soma(numero1, numero2);
+                                break calculadora;
+                            }
+                            case 2 -> {
+                                Calculadora.subtracao(numero1, numero2);
+                                break calculadora;
+                            }
+                            case 3 -> {
+                                Calculadora.multiplicacao(numero1, numero2);
+                                break calculadora;
+                            }
+                            case 4 -> {
+                                Calculadora.divisao(numero1, numero2);
+                                break calculadora;
+                            }
+                            case 5 ->{
+                                break calculadora;
+                            }
+                            default -> System.out.println("Seleção inválida");
+                        }
+                    }
+                    System.out.println();
                 }
                 case 2 -> {
                     // Mensagem
                     System.out.println("Exercício mensagem");
+                    System.out.println();
                     Mensagem.obterMensagem();
+                    System.out.println();
                 }
                 case 3 -> {
                     // Empréstimo
@@ -45,58 +78,74 @@ public class Main {
                     valorUsuario = scan.nextDouble();
                     System.out.println("Numero de parcelas (2 ou 3): ");
                     parcelas = scan.nextInt();
+                    System.out.println();
                     Emprestimo.calcular(valorUsuario, parcelas);
+                    System.out.println();
                 }
                 case 4 -> {
                     // Área quadrilatero
                     System.out.println("Exercício quadrilátero");
-                    System.out.println("Selecione a forma geométrica cuja área será calculada: ");
-                    System.out.println("1 - Quadrado");
-                    System.out.println("2 - Retângulo");
-                    System.out.println("3 - Trapézio");
-                    System.out.println("4 - Losango");
-                    System.out.println("5 - Retornar para o menu anterior");
-                    int opcaoQuad = scan.nextInt();
-                    switch (opcaoQuad) {
-                        case 1 -> {
-                            double lado;
-                            System.out.println("Digite o valor do lado do quadrado: ");
-                            lado = scan.nextDouble();
-                            Quadrilatero.area(lado);
-                            break inicio;
-                        }
-                        case 2 -> {
-                            double lado1, lado2;
-                            System.out.println("Digite o valor do primeiro lado do retangulo: ");
-                            lado1 = scan.nextDouble();
-                            System.out.println("Digite o valor do segundo lado do retangulo: ");
-                            lado2 = scan.nextDouble();
-                            Quadrilatero.area(lado1, lado2);
-                            break inicio;
-                        }
-                        case 3 -> {
-                            double baseMaior, baseMenor, altura;
-                            System.out.println("Digite o valor da base maior do trapézio: ");
-                            baseMaior = scan.nextDouble();
-                            System.out.println("Digite o valor da base menor do trapézio: ");
-                            baseMenor = scan.nextDouble();
-                            System.out.println("Digite o valor da altura do trapézio: ");
-                            altura = scan.nextDouble();
-                            Quadrilatero.area(baseMaior, baseMenor, altura);
-                            break inicio;
-                        }
-                        case 4 -> {
-                            float diagonal1, diagonal2;
-                            System.out.println("Digite o valor da primeira diagonal do losango: ");
-                            diagonal1 = scan.nextFloat();
-                            System.out.println("Digite o valor da segunda diagonal do losango: ");
-                            diagonal2 = scan.nextFloat();
-                            Quadrilatero.area(diagonal1, diagonal2);
-                            break inicio;
-                        }
-                        default -> {
-                            System.out.println("Opção inválida");
-                            break inicio;
+                    quadrilatero:while(true){
+                        System.out.println("Selecione a forma geométrica cuja área será calculada: ");
+                        System.out.println("1 - Quadrado");
+                        System.out.println("2 - Retângulo");
+                        System.out.println("3 - Trapézio");
+                        System.out.println("4 - Losango");
+                        System.out.println("5 - Retornar ao menu anterior");
+                        int opcaoQuad = scan.nextInt();
+                        switch (opcaoQuad) {
+                            case 1 -> {
+                                double lado;
+                                System.out.println("Digite o valor do lado do quadrado: ");
+                                lado = scan.nextDouble();
+                                double areaQuadrado=Quadrilatero.area(lado);
+                                System.out.println();
+                                System.out.println("Área do quadrado: "+areaQuadrado);
+                                System.out.println();
+                            }
+                            case 2 -> {
+                                double lado1, lado2;
+                                System.out.println("Digite o valor do primeiro lado do retangulo: ");
+                                lado1 = scan.nextDouble();
+                                System.out.println("Digite o valor do segundo lado do retangulo: ");
+                                lado2 = scan.nextDouble();
+                                double areaRetangulo=Quadrilatero.area(lado1, lado2);
+                                System.out.println();
+                                System.out.println("Área do retângulo:" + areaRetangulo);
+                                System.out.println();
+                            }
+                            case 3 -> {
+                                double baseMaior, baseMenor, altura;
+                                System.out.println("Digite o valor da base maior do trapézio: ");
+                                baseMaior = scan.nextDouble();
+                                System.out.println("Digite o valor da base menor do trapézio: ");
+                                baseMenor = scan.nextDouble();
+                                System.out.println("Digite o valor da altura do trapézio: ");
+                                altura = scan.nextDouble();
+                                double areaTrapezio=Quadrilatero.area(baseMaior, baseMenor, altura);
+                                System.out.println();
+                                System.out.println("Área do trapézio:" + areaTrapezio);
+                                System.out.println();
+                            }
+                            case 4 -> {
+                                float diagonal1, diagonal2;
+                                System.out.println("Digite o valor da primeira diagonal do losango: ");
+                                diagonal1 = scan.nextFloat();
+                                System.out.println("Digite o valor da segunda diagonal do losango: ");
+                                diagonal2 = scan.nextFloat();
+                                float areaLosango=Quadrilatero.area(diagonal1, diagonal2);
+                                System.out.println();
+                                System.out.println("Área do losango:" + areaLosango);
+                                System.out.println();
+                            }
+                            case 5 ->{
+                                break quadrilatero;
+                            }
+                            default -> {
+                                System.out.println();
+                                System.out.println("Opção inválida");
+                                System.out.println();
+                            }
                         }
 
                     }
@@ -104,13 +153,9 @@ public class Main {
                 }
                 case 5 -> {
                     System.out.println("Encerrando aplicação...");
-                    shouldBreak=true;
                     break inicio;
                 }
-                default -> {
-                    System.out.println("Opção inválida.");
-                    break inicio;
-                }
+                default -> System.out.println("Opção inválida.");
             }
         }
     }
