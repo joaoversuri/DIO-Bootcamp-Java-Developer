805a0e85afc1041ee949027c2b542290a7dbb223
joaoversuri <joaopaulo.db.versuri@gmail.com>
2021-12-29 00:17:56 -0300

    Teste na IDE Eclipse.

--- teste-java-eclipse/src/br/com/dio/TesteJavaEclipse.java
+++ teste-java-eclipse/src/br/com/dio/TesteJavaEclipse.java
@@ -5,16 +5,46 @@ import br.com.dio.model.Gato;
 public class TesteJavaEclipse {
 
 	public static void main(String[] args) {
-		if ((10 + 15) < 20)
-		    System.out.println("Verdade");
-		else
-		    System.out.println("Mentira");
+		Gato gato = new Gato();
+        System.out.println(gato);
+
+        Livro livro = new Livro("O Senhor dos anéis: A Sociedade do Anel", 450);
+        System.out.println(livro);
 	}
 
 }
 
 
-class Livros{
-	private String nome;
-	private String npag;
+class Livro{
+    private String nome;
+    private Integer npag;
+
+    public Livro(String nome, Integer npag) {
+        this.nome = nome;
+        this.npag = npag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNpag() {
+        return npag;
+    }
+
+    public void setNpag(Integer npag) {
+        this.npag = npag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", npag='" + npag + '\'' +
+                '}';
+    }
 }
